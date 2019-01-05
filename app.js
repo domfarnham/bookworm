eb82beec307a3ad05053998d264f32db63567d88
Dominic Farnham <dominic.farnham@hotmail.co.uk>
2019-01-05 22:03:58 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path')
+
+// parse incoming requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// serve static files from /public
+app.use(express.static(path.join(__dirname, '/public')))
+
+// view engine setup
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '/views'))
+
+// include routes
+const routes = require('./routes/index')
+app.use('/', routes)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('File Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+// define as the last app.use callback
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+// listen on port 3000
+app.listen(3000, () => {
+  console.log('App is listening on port 3000')
+})
