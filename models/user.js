b48738e0342c135bb0ca18fc975219e2e812bf26
Dominic Farnham <dominic.farnham@hotmail.co.uk>
2019-01-06 19:19:29 +0000

    Added hashing of passwords before storing in DB

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -23,6 +24,17 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
+UserSchema.pre('save', function (next) {
+  var user = this
+  console.log('password is: ', user.password)
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err)
+    }
+    user.password = hash
+    next()
+  })
+})
 const User = mongoose.model('User', UserSchema)
 
 module.exports = User
