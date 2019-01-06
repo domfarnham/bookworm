204a5487e4c80d29902f4df65fc257d13d7ce8f1
Dominic Farnham <dominic.farnham@hotmail.co.uk>
2019-01-06 17:55:39 +0000

    Setup User model with Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
+
+mongoose.connect('mongodb://localhost:27017/bookworm')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
 
 // parse incoming requests
 app.use(bodyParser.json())
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 // GET /
 router.get('/', (req, res, next) => {
@@ -14,6 +15,28 @@ router.get('/about', (req, res, next) => {
 // GET /contact
 router.get('/contact', (req, res, next) => {
   return res.render('contact', { title: 'Contact' })
+})
+
+router.get('/register', (req, res, next) => {
+  return res.render('register', { title: 'Sign UP' })
+})
+
+router.post('/register', (req, res, next) => {
+  if (req.body.email &&
+    req.body.name &&
+    req.body.fovoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword) {
+    if (req.body.password !== req.body.confirmPassword) {
+      const err = new Error('Passwords do not match.')
+      err.status = 400
+      next(err)
+    }
+  } else {
+    const err = new Error('All fields required.')
+    err.status = 400
+    next(err)
+  }
 })
 
 module.exports = router
